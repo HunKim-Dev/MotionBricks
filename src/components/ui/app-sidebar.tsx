ee5172a178c74f45be2c7b29070e7517fdf16ee4
김태헌 <7w7w7w7w@naver.com>
2025-09-22 15:56:47 +0900

    feat: 사이드바 뼈대 컴포넌트 구현

---
+++ src/components/ui/app-sidebar.tsx
@@ -0,0 +1,39 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { Save, Upload } from "lucide-react";
+
+const AppSidebar = () => {
+  const tooltipButtons = [
+    { text: "저장", icon: Save },
+    { text: "불러오기", icon: Upload },
+  ];
+  return (
+    <Sidebar side="right" variant="sidebar">
+      <SidebarHeader className="px-3 py-4">
+        <div className="flex items-center gap-5 justify-end">
+          {tooltipButtons.map((tooltipButton) => (
+            <Tooltip key={tooltipButton.text}>
+              <TooltipTrigger>
+                <tooltipButton.icon className="mr-2 h-5 w-5" />
+              </TooltipTrigger>
+              <TooltipContent>{tooltipButton.text}</TooltipContent>
+            </Tooltip>
+          ))}
+        </div>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup />
+        <SidebarGroup />
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+};
+
+export default AppSidebar;
