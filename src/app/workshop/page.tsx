4aeed2ed5c8836e3167024e8748adb569024e063
김태헌 <7w7w7w7w@naver.com>
2025-09-24 02:10:39 +0900

    feat: 3d 환경 세팅 OrbitControls를 통한 카메라 회전/줌/이동 조작 구현

--- src/app/workshop/page.tsx
+++ src/app/workshop/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 const WorkShopPage = () => {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
       <Canvas camera={{ position: [140, 120, 220], fov: 45 }}>
         <gridHelper args={[600, 60]} />
+        <OrbitControls enableDamping />
       </Canvas>
     </div>
   );
