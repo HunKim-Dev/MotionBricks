aa9172e6ba0c6a43296bae140b32a56456dbb916
김태헌 <7w7w7w7w@naver.com>
2025-09-24 02:09:18 +0900

    feat: 3d 환경 세팅 그리드 구현

--- src/app/workshop/page.tsx
+++ src/app/workshop/page.tsx
@@ -1,5 +1,15 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+
 const WorkShopPage = () => {
-  return <h1>Bricks Render place</h1>;
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <Canvas camera={{ position: [140, 120, 220], fov: 45 }}>
+        <gridHelper args={[600, 60]} />
+      </Canvas>
+    </div>
+  );
 };
 
 export default WorkShopPage;
