828e4a95bd30fc5bebbcc14bab9dac077c21de7d
김태헌 <7w7w7w7w@naver.com>
2025-09-24 22:39:44 +0900

    feat: 브릭 렌더 컴포넌트  브릭 ui에 추가

--- src/app/workshop/page.tsx
+++ src/app/workshop/page.tsx
@@ -2,12 +2,14 @@
 
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import LDrawModel from "@/components/workshop/ldraw-model";
 
 const WorkShopPage = () => {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
       <Canvas camera={{ position: [140, 120, 220], fov: 45 }}>
         <gridHelper args={[600, 60]} />
+        <LDrawModel />
         <OrbitControls enableDamping />
       </Canvas>
     </div>
