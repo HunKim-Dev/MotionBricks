e8d34d7a3bb6bcd083abc80ab443ab30c56302cb
김태헌 <7w7w7w7w@naver.com>
2025-09-27 15:16:23 +0900

    refactor: 브릭 조명 세팅 상수로 변경

--- src/app/workshop/page.tsx
+++ src/app/workshop/page.tsx
@@ -3,14 +3,14 @@
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import LDrawModel from "@/components/workshop/ldraw-model";
-import { CAMERA_LOCATION, GRID_SIZE } from "config/brick-config";
+import { CAMERA_LOCATION, GRID_SIZE, BRICK_LIGHT } from "config/brick-config";
 
 const WorkShopPage = () => {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
       <Canvas camera={CAMERA_LOCATION}>
         <gridHelper args={GRID_SIZE} />
-        <ambientLight intensity={0.7} />
+        <ambientLight intensity={BRICK_LIGHT} />
         <LDrawModel />
         <OrbitControls enableDamping />
       </Canvas>
