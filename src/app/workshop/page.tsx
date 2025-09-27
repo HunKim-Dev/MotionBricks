2b240e023bb79ef2e3b147dbea04ed671cceb1f9
김태헌 <7w7w7w7w@naver.com>
2025-09-28 00:32:34 +0900

    feat: onPointerMissed 이벤트로 브릭 선택 해제 추가

--- src/app/workshop/page.tsx
+++ src/app/workshop/page.tsx
@@ -8,7 +8,12 @@ import { CAMERA_LOCATION, GRID_SIZE, BRICK_LIGHT } from "config/brick-config";
 const WorkShopPage = () => {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
-      <Canvas camera={CAMERA_LOCATION}>
+      <Canvas
+        camera={CAMERA_LOCATION}
+        onPointerMissed={() => {
+          window.dispatchEvent(new CustomEvent("handle-missed"));
+        }}
+      >
         <gridHelper args={GRID_SIZE} />
         <ambientLight intensity={BRICK_LIGHT} />
         <LDrawModel />
