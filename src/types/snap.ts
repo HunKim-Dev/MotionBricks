5c288c82e06cfda820fa4aa86438f153190dd654
김태헌 <7w7w7w7w@naver.com>
2025-10-09 21:42:44 +0900

    refactor: 스냅 관련 타입 정의 추가

--- src/types/snap.ts
+++ src/types/snap.ts
@@ -12,3 +12,26 @@ export type PartSnapData = {
   coordinateFrame: "part-local";
   snapPoints: SnapPoint[];
 };
+
+export type SnapPairMatch = {
+  movingBrickId: string;
+  targetBrickId: string;
+  worldOffset: [number, number, number];
+  errorXZ: number;
+};
+
+export type SnapCandidiate = {
+  targetBrickId: number;
+  matchedPairs: SnapPairMatch[];
+  matchCount: number;
+  totalStuds: number;
+  isFullStud: boolean;
+  averageError: number;
+};
+
+export type SnapOptions = {
+  maxXZ: number;
+  minDotY: number;
+};
+
+export type WorldOffset = [number, number, number];
