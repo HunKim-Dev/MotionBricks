e3291519d4efde493a8fc82f85de33df7f3c4863
김태헌 <7w7w7w7w@naver.com>
2025-10-15 16:21:03 +0900

    refactor: 그리드 ui 기반 관련 상수 추가

---
+++ config/ui-config.ts
@@ -0,0 +1,9 @@
+export const GRID_HOVER_Y_OFFSET = 0.001;
+
+export const GRID_HOVER_BAND_THICKNESS_STUDS = 17;
+
+export const GRID_HOVER_OPACITY = 0.25;
+
+export const GRID_HOVER_COLOR_X = "#4ade80";
+
+export const GRID_HOVER_COLOR_Z = "#60a5fa";
