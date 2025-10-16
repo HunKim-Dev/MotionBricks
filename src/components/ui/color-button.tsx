9bf5b8643af42c09e1772744d7b142e98248a2a4
김태헌 <7w7w7w7w@naver.com>
2025-10-16 14:32:28 +0900

    feat: 브릭 색상 변경 버튼 기능 구현

---
+++ src/components/ui/color-button.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Palette } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+import { useBrickPartsStore } from "@/store/brick-parts";
+
+const ColorBrickButton = () => {
+  const selectedBrickUuid = useBrickPartsStore((state) => state.selectedBrickUuid);
+
+  const hexColorPaletteClick = () => {
+    if (!selectedBrickUuid) return;
+
+    window.dispatchEvent(
+      new CustomEvent("open-color-picker", { detail: { uuid: selectedBrickUuid } })
+    );
+  };
+
+  return (
+    <div className="flex items-center justify-end px-2">
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            size="icon"
+            className="h-8 w-8 border-black text-black hover:bg-black/5 disabled:opacity-40"
+            variant="outline"
+            onClick={hexColorPaletteClick}
+          >
+            <Palette className="h-4 w-4" />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>Color</TooltipContent>
+      </Tooltip>
+    </div>
+  );
+};
+
+export default ColorBrickButton;
