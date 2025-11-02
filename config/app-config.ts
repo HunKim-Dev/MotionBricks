b25e91e4ed573b29ce14094730b5d08a1fd5e936
김태헌 <7w7w7w7w@naver.com>
2025-11-02 23:34:37 +0900

    refactor: 랜딩 페이지 관련 상수 추가

--- config/app-config.ts
+++ config/app-config.ts
@@ -1,1 +1,9 @@
-export const CAMERA_START_ERROR = "카메라 시작 실패" as string;
+export const CAMERA_START_ERROR = "카메라 시작 실패";
+
+export const LANDING_PAGE_TITLE = "MotionBricks";
+
+export const LANDING_PAGE_SUBTITLE = "손 제스처로 조립하는 3D 레고 시뮬레이터";
+
+export const LANDING_PAGE_LOGIN_LABEL = "로그인";
+
+export const LANDING_PAGE_FREE_TRIAL_LABEL = "무료 체험하기";
