04c15445d336dee27c44a045b18e2fa48394a7cb
김태헌 <7w7w7w7w@naver.com>
2025-11-06 17:45:16 +0900

    feat: 저장, 불러오기, 로그아웃 toaster 알림 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 import "./globals.css";
+import { Toaster } from "@/components/ui/sonner";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Toaster position="top-left" richColors />
+      </body>
     </html>
   );
 };
