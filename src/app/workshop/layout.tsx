86b9f050762a212b50b9f9b9ab630549e6f5b7fb
김태헌 <7w7w7w7w@naver.com>
2025-11-09 00:11:55 +0900

    feat: 전역 세션 관리 위한 session provider 추가

--- src/app/workshop/layout.tsx
+++ src/app/workshop/layout.tsx
@@ -1,20 +1,23 @@
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 import AppSidebar from "@/components/ui/app-sidebar";
 import BottomBrickDock from "@/components/ui/bottom-bricks-dock";
+import { SessionProvider } from "next-auth/react";
 
 const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
-    <SidebarProvider>
-      <div className="flex min-h-screen w-full">
-        <SidebarInset>
-          <main className="relative flex-1">
-            {children}
-            <BottomBrickDock />
-          </main>
-        </SidebarInset>
-        <AppSidebar />
-      </div>
-    </SidebarProvider>
+    <SessionProvider>
+      <SidebarProvider>
+        <div className="flex min-h-screen w-full">
+          <SidebarInset>
+            <main className="relative flex-1">
+              {children}
+              <BottomBrickDock />
+            </main>
+          </SidebarInset>
+          <AppSidebar />
+        </div>
+      </SidebarProvider>
+    </SessionProvider>
   );
 };
 
