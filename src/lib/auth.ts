32a239c521c40b7f8f8f92572c79dd7fafdba89f
김태헌 <7w7w7w7w@naver.com>
2026-01-02 02:09:49 +0900

    refactor: 서버 인증 로직 분리 및 jwt 세션 기반 인증 로직 구현

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,24 @@
 import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+import authConfig from "./auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google],
+  session: { strategy: "jwt" },
+  ...authConfig,
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.id = user.id as string;
+
+      return token;
+    },
+
+    async session({ session, token }) {
+      session.user.id = token.id as string;
+
+      return session;
+    },
+  },
 });
